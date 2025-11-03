0afaf6fe4180839408cb5743c9146a4b07a9c79a
Alexey Mokeev <work.alexey.mokeev@gmail.com>
2025-11-03 19:33:34 +0400

    minor fixes

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -40,14 +40,14 @@ func New(storagePath string) (*Storage, error) {
 	return &Storage{db: db}, nil
 }
 
-func (s *Storage) SaveURL(url string, alias string) (int64, error) {
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error) {
 	const op = "storage.sqlite.SaveURL()"
 	stmt, err := s.db.Prepare(`INSERT INTO url(url, alias) VALUES(?,?);`)
 	if err != nil {
 		return 0, fmt.Errorf("%s: %w", op, err)
 	}
 
-	res, err := stmt.Exec(url, alias)
+	res, err := stmt.Exec(urlToSave, alias)
 	if err != nil {
 		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
 			return 0, fmt.Errorf("%s:%w", op, storage.ErrURLExists)
